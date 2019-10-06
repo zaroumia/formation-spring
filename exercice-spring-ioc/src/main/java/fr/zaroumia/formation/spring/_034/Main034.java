9f159d661f3778b2bb18af710b8d036da353e4be
Momo <simo1705@gmail.com>
2019-10-06 13:43:03 +0200

    Add section : autowiring

---
+++ exercice-spring-ioc/src/main/java/fr/zaroumia/formation/spring/_034/Main034.java
@@ -0,0 +1,10 @@
+package fr.zaroumia.formation.spring._034;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main034 {
+	public static void main(final String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("appContext_034.xml");
+	}
+}
